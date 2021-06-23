175e420768220b91396c5331cfab255c30524878
Djaniza Vasques <djanizav@gmail.com>
2021-06-23 04:30:35 -0300

    Corrige data e Pokemon.js

--- exercises/introducao-a-react_1/my-pokedex/src/Components/Pokedex.js
+++ exercises/introducao-a-react_1/my-pokedex/src/Components/Pokedex.js
@@ -1,10 +1,11 @@
 import Pokemon from './Pokemon';
-import pokemons from 'data';
+import pokemons from '../data';
+import React from 'react';
 
-class Pokedex extends Component {
+class Pokedex extends React.Component {
   render() {
     return (
-      pokemons.map((poke) => <Pokemon pokemon={poke})        
+      pokemons.map((poke) => <Pokemon pokemon={poke} />)        
     );
   }
 }
--- exercises/introducao-a-react_1/my-pokedex/src/Components/Pokemon.js
+++ exercises/introducao-a-react_1/my-pokedex/src/Components/Pokemon.js
@@ -1,12 +1,14 @@
+import React from 'react';
+
 class Pokemon extends React.Component {
   render() {
     const { pokemon } = this.props;
     return (
     <div>
-      <img>{ pokemon.image }</img>;
+      <img src={ pokemon.image } alt={ pokemon.name}></img>;
       <h2>{ pokemon.name }</h2>;
-      <p>Pokemon do Tipo: { pokemon.type }</p>;
-      <p>Tamanho: { pokemon.averageWeight.value }{ pokemon.averageWeight.measurementUnit }</p>;
+      <p>Type: { pokemon.type }</p>;
+      <p>Size: { pokemon.averageWeight.value }{ pokemon.averageWeight.measurementUnit }</p>;
     </div>
     );
     }
